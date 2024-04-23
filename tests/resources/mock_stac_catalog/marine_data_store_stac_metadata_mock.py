2fc6642e1b205d1087f9c2100c600d032fea085d
renaudjester <renaud.jester@outlook.fr>
2024-04-23 15:19:40 +0200

    test: add mock and snapshots

---
+++ tests/resources/mock_stac_catalog/marine_data_store_stac_metadata_mock.py
@@ -0,0 +1,84 @@
+from typing import Optional
+
+from tests.resources.mock_stac_catalog.mock_catalog import MOCK_STAC_CATALOG
+from tests.resources.mock_stac_catalog.mock_dataset_GLO_glo_phy_cur import (
+    MOCK_DATASET_GLO_PHY_CUR,
+)
+from tests.resources.mock_stac_catalog.mock_dataset_GLO_glo_phy_cur_new_version import (
+    MOCK_DATASET_GLO_PHY_CUR_NEW_VERSION,
+)
+from tests.resources.mock_stac_catalog.mock_dataset_GLO_glo_phy_so import (
+    MOCK_DATASET_GLO_PHY_SO,
+)
+from tests.resources.mock_stac_catalog.mock_dataset_NWSHELF_P1D_m_202012 import (
+    MOCK_DATASET_NWSHELF_P1D_M_202012,
+)
+from tests.resources.mock_stac_catalog.mock_dataset_NWSHELF_P1M_m_202012 import (
+    MOCK_DATASET_NWSHELF_P1M_M_202012,
+)
+from tests.resources.mock_stac_catalog.mock_product_GLO import MOCK_PRODUCT_GLO
+from tests.resources.mock_stac_catalog.mock_product_NWSHELF import (
+    MOCK_PRODUCT_NWSHELF,
+)
+
+BASE_URL = "https://stac.marine.copernicus.eu/metadata"
+
+
+def mocked_stac_aiohttp_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data: Optional[dict], status_code: int):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        async def json(self) -> Optional[dict]:
+            return self.json_data
+
+        async def __aenter__(self):
+            return self
+
+        async def __aexit__(self, exc_type, exc_val, exc_tb):
+            pass
+
+    if args[0] == f"{BASE_URL}/catalog.stac.json":
+        return MockResponse(MOCK_STAC_CATALOG, 200)
+    elif (
+        args[0]
+        == f"{BASE_URL}/GLOBAL_ANALYSISFORECAST_PHY_001_024/product.stac.json"
+    ):
+        return MockResponse(MOCK_PRODUCT_GLO, 200)
+    elif (
+        args[0]
+        == f"{BASE_URL}/NWSHELF_MULTIYEAR_BGC_004_011/product.stac.json"
+    ):
+        return MockResponse(MOCK_PRODUCT_NWSHELF, 200)
+    elif (
+        args[0] == f"{BASE_URL}/GLOBAL_ANALYSISFORECAST_PHY_001_024/"
+        f"cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_202211/"
+        f"dataset.stac.json"
+    ):
+        return MockResponse(MOCK_DATASET_GLO_PHY_CUR, 200)
+    elif (
+        args[0] == f"{BASE_URL}/GLOBAL_ANALYSISFORECAST_PHY_001_024/"
+        f"cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m_206011/"
+        f"dataset.stac.json"
+    ):
+        return MockResponse(MOCK_DATASET_GLO_PHY_CUR_NEW_VERSION, 200)
+    elif (
+        args[0] == f"{BASE_URL}/GLOBAL_ANALYSISFORECAST_PHY_001_024/"
+        f"cmems_mod_glo_phy-so_anfc_0.083deg_P1D-m_202211/"
+        f"dataset.stac.json"
+    ):
+        return MockResponse(MOCK_DATASET_GLO_PHY_SO, 200)
+    elif (
+        args[0] == f"{BASE_URL}/NWSHELF_MULTIYEAR_BGC_004_011/"
+        f"cmems_mod_nws_bgc-chl_my_7km-3D_P1D-m_202012/"
+        f"dataset.stac.json"
+    ):
+        return MockResponse(MOCK_DATASET_NWSHELF_P1D_M_202012, 200)
+    elif (
+        args[0] == f"{BASE_URL}/NWSHELF_MULTIYEAR_BGC_004_011/"
+        f"cmems_mod_nws_bgc-chl_my_7km-3D_P1M-m_202012/"
+        f"dataset.stac.json"
+    ):
+        return MockResponse(MOCK_DATASET_NWSHELF_P1M_M_202012, 200)
+    return MockResponse(None, 404)
---
+++ tests/test_describe_released_date.py
@@ -0,0 +1,49 @@
+from unittest import mock
+
+from copernicusmarine import describe
+from tests.resources.mock_stac_catalog.marine_data_store_stac_metadata_mock import (
+    mocked_stac_aiohttp_get,
+)
+
+
+class TestDescribe:
+    @mock.patch(
+        "aiohttp.ClientSession.get",
+        side_effect=mocked_stac_aiohttp_get,
+    )
+    def when_I_describe_the_marine_data_store(
+        self,
+        mock_get,
+        include_all_versions=False,
+    ):
+        return describe(
+            no_metadata_cache=True,
+            include_all_versions=include_all_versions,
+            include_datasets=True,
+        )
+
+    def test_only_released_dataset_by_default(self, snapshot):
+        describe_result = self.when_I_describe_the_marine_data_store()
+        self.then_I_dont_get_the_not_released_products_version_and_datasets(
+            describe_result, snapshot
+        )
+
+    def then_I_dont_get_the_not_released_products_version_and_datasets(
+        self, describe_result, snapshot
+    ):
+        assert 1 == len(describe_result["products"])
+        assert describe_result == snapshot
+
+    def test_describe_all_versions(self, snapshot):
+        describe_result = self.when_I_describe_the_marine_data_store(
+            include_all_versions=True
+        )
+        self.then_I_get_all_products_versions_and_datasets(
+            describe_result, snapshot
+        )
+
+    def then_I_get_all_products_versions_and_datasets(
+        self, describe_result, snapshot
+    ):
+        assert 2 == len(describe_result["products"])
+        assert describe_result == snapshot
