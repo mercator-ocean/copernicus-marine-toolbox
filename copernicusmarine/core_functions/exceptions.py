8a7ffc965fe60d90c70ceaaa1cfd7a4e2a110001
uriii3 <oriolrv689@gmail.com>
2024-05-02 15:06:14 +0200

    Add message into error handling

--- copernicusmarine/core_functions/exceptions.py
+++ copernicusmarine/core_functions/exceptions.py
@@ -6,12 +6,12 @@ class VariableDoesNotExistInTheDataset(Exception):
     def __init__(self, variable):
         super().__init__(
             f"The variable '{variable}' is neither a variable or a standard name in"
-            f" the dataset"
+            f" the dataset."
         )
         self.__setattr__(
             "custom_exception_message",
             f"The variable '{variable}' is neither a variable or a standard name in "
-            f"the dataset",
+            f"the dataset.",
         )
 
 
