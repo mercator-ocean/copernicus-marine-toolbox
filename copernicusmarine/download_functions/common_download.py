d1114c23f6a7ab00738ffe02e063321f982ad8ab
Oriol Ricart Vilarrubias <45894267+uriii3@users.noreply.github.com>
2024-05-07 17:04:16 +0200

    default _Fillvalue attribute (#60)
    Merging this one!

--- copernicusmarine/download_functions/common_download.py
+++ copernicusmarine/download_functions/common_download.py
@@ -1,6 +1,6 @@
 import logging
 import pathlib
-from typing import Optional
+from typing import Any, Optional
 
 import xarray
 import zarr
@@ -57,17 +57,23 @@ def _prepare_download_dataset_as_netcdf(
     netcdf3_compatible: bool,
 ):
     logger.debug("Writing dataset to NetCDF")
+    encoding: dict[str, Any]
+    encoding = {f"{coord}": {"_FillValue": None} for coord in dataset.coords}
     if netcdf_compression_enabled:
         complevel = (
             1 if netcdf_compression_level is None else netcdf_compression_level
         )
         logger.info(f"NetCDF compression enabled with level {complevel}")
         comp = dict(
-            zlib=True, complevel=complevel, contiguous=False, shuffle=True
+            zlib=True,
+            complevel=complevel,
+            contiguous=False,
+            shuffle=True,
+            _FillValue=None,
         )
-        encoding = {var: comp for var in dataset.data_vars}
-    else:
-        encoding = None
+        encoding_vars = {var: comp for var in dataset.data_vars}
+        encoding.update(encoding_vars)
+
     xarray_download_format = "NETCDF3_CLASSIC" if netcdf3_compatible else None
     return dataset.to_netcdf(
         output_path,
