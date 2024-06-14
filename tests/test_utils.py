5db3aa0f63d3e209960d9fb859165dc8ecbe6fbc
renaudjester <renaud.jester@outlook.fr>
2024-06-14 09:13:36 +0200

    tests: update to stderr

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ import logging
 import subprocess
 import time
 from subprocess import CompletedProcess
+from typing import Optional
 
 logger = logging.getLogger()
 
@@ -23,11 +24,12 @@ FIVE_MINUTES = 5 * 60
 def execute_in_terminal(
     command: list[str],
     timeout_second: float = FIVE_MINUTES,
-    input=None,
-    env=None,
+    input: Optional[bytes] = None,
+    env: Optional[dict[str, str]] = None,
 ) -> CompletedProcess[bytes]:
     t1 = time.time()
-    logger.info(f"Running command: {' '.join(command)}...")
+    command_to_print = " ".join([str(c) for c in command])
+    logger.info(f"Running command: {command_to_print}...")
     output = subprocess.run(
         command,
         capture_output=True,
@@ -37,7 +39,5 @@ def execute_in_terminal(
     )
     t2 = time.time()
     duration_second = t2 - t1
-    logger.info(
-        f"Command executed in {duration_second} s: {' '.join(command)}"
-    )
+    logger.info(f"Command executed in {duration_second} s: {command_to_print}")
     return output
