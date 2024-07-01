1dc4d32b7f0f5767c16b376fc3d6d102cd6130f6
renaudjester <renaud.jester@outlook.fr>
2024-07-01 12:46:53 +0200

    test: use tmp file for direct download tests

--- tests/test_get_direct_download.py
+++ tests/test_get_direct_download.py
@@ -24,7 +24,7 @@ DIRECT_DOWNLOAD_FAILS_BUT_LISTING_SUCCEEDS = (
 
 
 class TestGetDirectDownload:
-    def test_get_direct_download_file_list(self):
+    def test_get_direct_download_file_list(self, tmp_path):
         self.command = [
             "copernicusmarine",
             "get",
@@ -35,6 +35,8 @@ class TestGetDirectDownload:
             "--force-download",
             "--show-outputnames",
             "--overwrite-output-data",
+            "-o",
+            str(tmp_path),
         ]
         self.output = execute_in_terminal(self.command)
         assert (
@@ -43,8 +45,12 @@ class TestGetDirectDownload:
             b"history/BO/AR_PR_BO_58JM.nc"
         ) in self.output.stdout
         assert b"Skipping" not in self.output.stdout
-        self._assert_insitu_file_exists_locally("history/BO/AR_PR_BO_58JM.nc")
-        self._assert_insitu_file_exists_locally("history/BO/AR_PR_BO_58US.nc")
+        self._assert_insitu_file_exists_locally(
+            tmp_path, "history/BO/AR_PR_BO_58JM.nc"
+        )
+        self._assert_insitu_file_exists_locally(
+            tmp_path, "history/BO/AR_PR_BO_58US.nc"
+        )
         assert self.output.returncode == 0
 
     # Mocking to skip the listing and search phase
@@ -53,7 +59,7 @@ class TestGetDirectDownload:
         return_value=None,
     )
     def test_get_direct_download_list_file_only_one_not_found(
-        self, mock_download_header
+        self, mock_download_header, tmp_path
     ):
         self.command = [
             "copernicusmarine",
@@ -63,6 +69,8 @@ class TestGetDirectDownload:
             "--file-list",
             DIRECT_DOWNLOAD_FILE_LIST_EXAMPLE_WITH_ONE_WRONG,
             "--force-download",
+            "-o",
+            str(tmp_path),
         ]
         self.output = execute_in_terminal(self.command)
         assert (
@@ -74,10 +82,12 @@ class TestGetDirectDownload:
         assert (
             b"history/BO/AR_PR_BO_58JM.nc not found on the server. Skipping."
         ) not in self.output.stdout
-        self._assert_insitu_file_exists_locally("history/BO/AR_PR_BO_58JM.nc")
+        self._assert_insitu_file_exists_locally(
+            tmp_path, file_name="history/BO/AR_PR_BO_58JM.nc"
+        )
         assert self.output.returncode == 0
 
-    def test_get_direct_download_different_path_types(self):
+    def test_get_direct_download_different_path_types(self, tmp_path):
         self.command = [
             "copernicusmarine",
             "get",
@@ -86,13 +96,15 @@ class TestGetDirectDownload:
             "--file-list",
             DIRECT_DOWNLOAD_FILE_LIST_EXAMPLE_DIIFERENT_PATH_TYPES,
             "--force-download",
+            "-o",
+            str(tmp_path),
         ]
         self.output = execute_in_terminal(self.command)
         assert b"WARNING" not in self.output.stdout
         assert b"Skipping" not in self.output.stdout
         assert self.output.returncode == 0
 
-    def test_get_direct_download_fails_but_listing_succeeds(self):
+    def test_get_direct_download_fails_but_listing_succeeds(self, tmp_path):
         self.command = [
             "copernicusmarine",
             "get",
@@ -101,6 +113,8 @@ class TestGetDirectDownload:
             "--file-list",
             DIRECT_DOWNLOAD_FAILS_BUT_LISTING_SUCCEEDS,
             "--force-download",
+            "-o",
+            str(tmp_path),
         ]
         self.output = execute_in_terminal(self.command)
         assert b"Skipping" in self.output.stdout
@@ -109,40 +123,49 @@ class TestGetDirectDownload:
             in self.output.stdout
         )
         assert os.path.exists(
-            "IBI_MULTIYEAR_PHY_005_002/"
-            "cmems_mod_ibi_phy_my_0.083deg-3D_P1M-m_202012/"
-            "2021/"
-            "CMEMS_v5r1_IBI_PHY_MY_PdE_01mav_20211001_20211031_R20230101_RE01.nc"
+            f"{tmp_path}/"
+            f"IBI_MULTIYEAR_PHY_005_002/"
+            f"cmems_mod_ibi_phy_my_0.083deg-3D_P1M-m_202012/"
+            f"2021/"
+            f"CMEMS_v5r1_IBI_PHY_MY_PdE_01mav_20211001_20211031_R20230101_RE01.nc"
         )
         assert self.output.returncode == 0
 
     # Python interface tests
 
-    def test_get_direct_download_file_list_python(self):
+    def test_get_direct_download_file_list_python(self, tmp_path):
         get_result = get(
             dataset_id="cmems_obs-ins_glo_phybgcwav_mynrt_na_irr",
             file_list=pathlib.Path(DIRECT_DOWNLOAD_FILE_LIST_EXAMPLE),
             force_download=True,
             show_outputnames=True,
             overwrite_output_data=True,
+            output_directory=tmp_path,
         )
         assert set(get_result) == {
             pathlib.Path(
-                "INSITU_GLO_PHYBGCWAV_DISCRETE_MYNRT_013_030/"
-                "cmems_obs-ins_glo_phybgcwav_mynrt_na_irr_202311/"
-                "history/BO/AR_PR_BO_58JM.nc"
+                f"{tmp_path}/"
+                f"INSITU_GLO_PHYBGCWAV_DISCRETE_MYNRT_013_030/"
+                f"cmems_obs-ins_glo_phybgcwav_mynrt_na_irr_202311/"
+                f"history/BO/AR_PR_BO_58JM.nc"
             ),
             pathlib.Path(
-                "INSITU_GLO_PHYBGCWAV_DISCRETE_MYNRT_013_030/"
-                "cmems_obs-ins_glo_phybgcwav_mynrt_na_irr_202311/"
-                "history/BO/AR_PR_BO_58US.nc"
+                f"{tmp_path}/"
+                f"INSITU_GLO_PHYBGCWAV_DISCRETE_MYNRT_013_030/"
+                f"cmems_obs-ins_glo_phybgcwav_mynrt_na_irr_202311/"
+                f"history/BO/AR_PR_BO_58US.nc"
             ),
         }
         for file_path in get_result:
             assert os.path.exists(file_path)
 
-    def _assert_insitu_file_exists_locally(self, file_name: str):
+    def _assert_insitu_file_exists_locally(
+        self,
+        temp_path,
+        file_name: str,
+    ):
         assert os.path.exists(
+            f"{temp_path}/"
             f"INSITU_GLO_PHYBGCWAV_DISCRETE_MYNRT_013_030/"
             f"cmems_obs-ins_glo_phybgcwav_mynrt_na_irr_202311/"
             f"{file_name}"
