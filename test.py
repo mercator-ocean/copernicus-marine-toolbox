e844851cef4ccf1f4c9ffb393deec6d5fb5a252e
renaudjester <renaud.jester@outlook.fr>
2024-07-25 15:56:19 +0200

    fix: consider 180 and -180 as the same point

--- test.py
+++
@@ -1,9 +0,0 @@
-import copernicusmarine
-
-dataset = copernicusmarine.open_dataset(
-    dataset_id="cmems_mod_glo_phy-thetao_anfc_0.083deg_P1D-m",
-    maximum_longitude=190,
-    minimum_longitude=-150,
-)
-
-print(dataset)
