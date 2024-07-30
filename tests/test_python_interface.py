ab5903c623969a18c3a7fed89e59d4e71db32b36
uriii3 <oriolrv689@gmail.com>
2024-07-30 15:36:23 +0200

    fix: adding again the positive attribute in the depth dimension

--- tests/test_python_interface.py
+++ tests/test_python_interface.py
@@ -227,6 +227,7 @@ class TestPythonInterface:
         dataset = open_dataset(
             dataset_id="cmems_mod_arc_phy_anfc_6km_detided_P1D-m"
         )
+        assert dataset.depth.attrs["positive"] == "down"
         assert dataset.depth.attrs["standard_name"] == "depth"
         assert dataset.depth.attrs["long_name"] == "Depth"
 
