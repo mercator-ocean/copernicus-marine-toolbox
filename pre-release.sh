d4e3a1c7255d4ecf5c2e35b34d42c0f717763daf
renaudjester <renaud.jester@outlook.fr>
2024-08-19 17:28:12 +0200

    chore: add way to do pre releases

---
+++ pre-release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eufo pipefail
+
+PRE_RELEASE_BRANCH=$(git branch --show-current)
+
+if [ -z `git status --porcelain` ] && [ ! -z "${BUMP_TYPE}" ] && [[ "${BUMP_TYPE}" == pre* ]]; then
+  RELEASE_BRANCH_NAME="New-copernicusmarine-package-pre-release"
+  git checkout -b $RELEASE_BRANCH_NAME
+  poetry version ${BUMP_TYPE}
+  VERSION=$(poetry version --short)
+  if [ "${PRE_RELEASE_BRANCH}" != "pre-releases/${VERSION}" ]; then
+    echo "Branch name should be pre-releases/${VERSION} instead of ${PRE_RELEASE_BRANCH}"
+    exit 1
+  fi
+  RELEASE_TITLE="Copernicus Marine Pre-Release ${VERSION}"
+  git commit -am "$RELEASE_TITLE"
+  git push --set-upstream origin $RELEASE_BRANCH_NAME
+  PR_LINK=$(gh pr create --title "$RELEASE_TITLE" --body "")
+  gh pr view $PR_LINK --web
+else
+  git status
+  echo "Release aborted. Clean your repository and use make pre-release-[BUMP_TYPE]."
+fi
