06911df3877cd4c1b7d12b2605a6f2952f36f971
Renaud <38732257+renaudjester@users.noreply.github.com>
2024-10-11 17:56:20 +0200

    chore: add validity check for logging (#161)
    Add a functionality to check if the user's credentials are valid. 
    Deletes the `--skip-if-logged-in` functionality

--- tests/test_command_line_interface.py
+++ tests/test_command_line_interface.py
@@ -5,7 +5,6 @@ import logging
 import os
 import pathlib
 import re
-import shutil
 from dataclasses import dataclass
 from json import loads
 from pathlib import Path
@@ -33,17 +32,6 @@ def get_all_files_in_folder_tree(folder: str) -> list[str]:
         for filename in files:
             downloaded_files.append(filename)
     return downloaded_files
-
-
-def get_environment_without_crendentials():
-    environment_without_crendentials = os.environ.copy()
-    environment_without_crendentials.pop(
-        "COPERNICUSMARINE_SERVICE_USERNAME", None
-    )
-    environment_without_crendentials.pop(
-        "COPERNICUSMARINE_SERVICE_PASSWORD", None
-    )
-    return environment_without_crendentials
 
 
 def get_file_size(filepath):
@@ -783,122 +771,6 @@ class TestCommandLineInterface:
         assert self.output.returncode == 0
         assert is_file
 
-    def test_process_is_stopped_when_credentials_are_invalid(self):
-        dataset_id = "cmems_mod_ibi_phy_my_0.083deg-3D_P1Y-m"
-
-        command = [
-            "copernicusmarine",
-            "subset",
-            "--username",
-            "toto",
-            "--password",
-            "tutu",
-            "--dataset-id",
-            f"{dataset_id}",
-            "--variable",
-            "thetao",
-            "--force-download",
-        ]
-
-        self.output = execute_in_terminal(command)
-
-        assert self.output.returncode == 1
-        assert b"Invalid username or password" in self.output.stderr
-
-    def test_login_is_prompt_when_configuration_file_doest_not_exist(
-        self, tmp_path
-    ):
-        dataset_id = "cmems_mod_ibi_phy_my_0.083deg-3D_P1Y-m"
-        credentials_file = Path(tmp_path, "i_do_not_exist")
-
-        environment_without_crendentials = (
-            get_environment_without_crendentials()
-        )
-
-        command = [
-            "copernicusmarine",
-            "subset",
-            "--dataset-id",
-            f"{dataset_id}",
-            "--variable",
-            "thetao",
-            "--minimum-longitude",
-            "-9.9",
-            "--maximum-longitude",
-            "-9.6",
-            "--minimum-latitude",
-            "33.96",
-            "--maximum-latitude",
-            "34.2",
-            "--minimum-depth",
-            "0.5",
-            "--maximum-depth",
-            "1.6",
-            "--credentials-file",
-            f"{credentials_file}",
-        ]
-
-        self.output = execute_in_terminal(
-            command, env=environment_without_crendentials
-        )
-        assert self.output.returncode == 1
-        assert (
-            b"Downloading Copernicus Marine data requires a Copernicus Marine username "
-            b"and password, sign up for free at:"
-            b" https://data.marine.copernicus.eu/register"
-        ) in self.output.stderr
-        assert b"copernicusmarine username:" in self.output.stdout
-
-    def test_login_command(self, tmp_path):
-        self.check_credentials_username_specified_password_prompt(tmp_path)
-
-    def check_credentials_username_specified_password_prompt(self, tmp_path):
-        assert os.getenv("COPERNICUSMARINE_SERVICE_USERNAME") is not None
-        assert os.getenv("COPERNICUSMARINE_SERVICE_PASSWORD") is not None
-
-        environment_without_crendentials = (
-            get_environment_without_crendentials()
-        )
-
-        command = [
-            "copernicusmarine",
-            "subset",
-            "--dataset-id",
-            "cmems_mod_glo_phy-thetao_anfc_0.083deg_P1D-m",
-            "--variable",
-            "thetao",
-            "--start-datetime",
-            "2023-04-26 00:00:00",
-            "--end-datetime",
-            "2023-04-28 23:59:59",
-            "--minimum-longitude",
-            "-9.8",
-            "--maximum-longitude",
-            "-4.8",
-            "--minimum-latitude",
-            "33.9",
-            "--maximum-latitude",
-            "38.0",
-            "--minimum-depth",
-            "9.573",
-            "--maximum-depth",
-            "11.4",
-            "--username",
-            f"{os.getenv('COPERNICUSMARINE_SERVICE_USERNAME')}",
-            "--force-download",
-            "-o",
-            f"{tmp_path}",
-        ]
-        password = os.getenv("COPERNICUSMARINE_SERVICE_PASSWORD")
-        assert password is not None
-        self.output = execute_in_terminal(
-            command,
-            env=environment_without_crendentials,
-            input=bytes(password, "utf-8"),
-        )
-        assert self.output.returncode == 0, self.output.stderr
-        shutil.rmtree(Path(tmp_path))
-
     def test_get_download_s3_with_wildcard_filter(self, tmp_path):
         filter = "*_200[123]*.nc"
         dataset_id = "cmems_mod_ibi_phy_my_0.083deg-3D_P1Y-m"
@@ -967,36 +839,8 @@ class TestCommandLineInterface:
         assert b"No data to download" in self.output.stderr
         assert self.output.returncode == 0
 
-    def test_login(self, tmp_path):
-        non_existing_directory = Path(tmp_path, "i_dont_exist")
-        command = [
-            "copernicusmarine",
-            "login",
-            "--overwrite-configuration-file",
-            "--configuration-file-directory",
-            f"{non_existing_directory}",
-            "--username",
-            f"{os.getenv('COPERNICUSMARINE_SERVICE_USERNAME')}",
-            "--password",
-            f"{os.getenv('COPERNICUSMARINE_SERVICE_PASSWORD')}",
-        ]
-
-        self.output = execute_in_terminal(command)
-        assert self.output.returncode == 0
-        assert non_existing_directory.is_dir()
-
-        command_with_skip = [
-            "copernicusmarine",
-            "login",
-            "--configuration-file-directory",
-            f"{non_existing_directory}",
-            "--skip-if-user-logged-in",
-        ]
-        output_with_skip = execute_in_terminal(command_with_skip)
-        assert output_with_skip.returncode == 0
-
     # TODO: separate tests for each service
-    # SUBSET, GET, LOGIN, DESCRIBE
+    # SUBSET, GET, DESCRIBE
     def test_subset_error_when_forced_service_does_not_exist(self):
         self.when_I_run_copernicus_marine_subset_forcing_a_service_not_available()
         self.then_I_got_a_clear_output_with_available_service_for_subset()
