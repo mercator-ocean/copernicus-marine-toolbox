4748e535cc00d5a01e65dc3bba983aa014ed2401
renaudjester <renaud.jester@outlook.fr>
2024-11-20 15:54:35 +0100

    test: nox with only one python

--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,14 @@
 import nox
 
 # Define the supported versions of Python and xarray
-PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
+# PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
 XARRAY_VERSIONS = ["2023.4.0", "latest"]
 DASK_VERSIONS = ["2022.1.0", "latest"]
 BOTO3_VERSIONS = ["1.26.0", "latest"]
-NUMPY_VERSIONS = ["1.22.0", ">=2.0.0"]
+NUMPY_VERSIONS = ["1.26.0", ">=2.0.0"]
 
 
-@nox.session(python=PYTHON_VERSIONS)
+# @nox.session(python=PYTHON_VERSIONS)
 @nox.parametrize("xarray_version", XARRAY_VERSIONS)
 @nox.parametrize("dask_version", DASK_VERSIONS)
 @nox.parametrize("boto3_version", BOTO3_VERSIONS)
@@ -33,11 +33,11 @@ def tests(session, xarray_version, dask_version, boto3_version, numpy_version):
         "pytest==7.4.0",
     )
 
-    # Install the library itself (optional, if you're testing a local package)
-    session.install(".")
+    # # Install the library itself (optional, if you're testing a local package)
+    # session.install(".")
 
     # Run tests
-    session.run("pytest tests_dependencie_versions/test_basic_commands.py")
+    session.run("pytest", "tests_dependencie_versions/test_basic_commands.py")
 
 
 def format_to_correct_pip_command(package_name: str, version: str):
