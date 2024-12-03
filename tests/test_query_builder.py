9aaf8fc89bbcc161efe5401eee94a56120e5a167
uriii3 <oriolrv689@gmail.com>
2024-12-03 15:24:23 +0100

    all snapshots

--- tests/test_query_builder.py
+++ tests/test_query_builder.py
@@ -4,184 +4,58 @@ from copernicusmarine.catalogue_parser.models import CopernicusMarineCatalogue
 from copernicusmarine.core_functions.fields_query_builder import (
     _return_available_fields,
     build_query,
-    get_queryable_requested_fields,
 )
 from copernicusmarine.core_functions.models import ResponseGet, ResponseSubset
-
-ALL_FIELDS = {
-    "COPERNICUS_MARINE_CATALOGUE": {
-        "products",  # list of product attr
-        "title",
-        "bbox",
-        "chunk_geometric_factor",
-        "chunk_reference_coordinate",
-        "chunk_type",
-        "chunking_length",
-        "coordinate_id",
-        "coordinate_unit",
-        "coordinates",
-        "dataset_id",
-        "dataset_name",
-        "datasets",
-        "description",
-        "digital_object_identifier",
-        "keywords",
-        "label",
-        "maximum_value",
-        "minimum_value",
-        "name",
-        "parts",
-        "processing_level",
-        "product_id",
-        "production_center",
-        "released_date",
-        "retired_date",
-        "service_format",
-        "service_name",
-        "service_short_name",
-        "services",
-        "short_name",
-        "sources",
-        "standard_name",
-        "step",
-        "thumbnail_url",
-        "units",
-        "uri",
-        "values",
-        "variables",
-        "versions",
-    },
-    "RESPONSE_GET": {
-        "files",  # following the fields of files:
-        "s3_url",
-        "https_url",
-        "file_size",
-        "last_modified_datetime",
-        "etag",
-        "file_format",
-        "output_directory",
-        "filename",
-        "file_path",
-        "file_status",
-        # We follow RepsonseGet attrs
-        "files_deleted",
-        "files_not_found",
-        "total_size",
-        "status",
-        "message",
-    },
-    "RESPONSE_SUBSET": {
-        "file_path",
-        "output_directory",
-        "filename",
-        "file_size",
-        "data_transfer_size",
-        "variables",
-        "coordinates_extent",  # fields in coordinates extent
-        "minimum",
-        "maximum",
-        "unit",
-        "coordinate_id",
-        # lastly the last part of the RepsonseSubset
-        "status",
-        "message",
-        "file_status",
-    },
-}
 
 
 class TestQueryBuilder:
     # For the catalogue
-    def test_query_builder_catalogue(self):
+    def test_query_builder_catalogue(self, snapshot):
         query = build_query(
             ({"product_id", "dataset_id"}), CopernicusMarineCatalogue
         )
-        assert query == {
-            "products": {
-                "__all__": {
-                    "product_id": True,
-                    "datasets": {"__all__": {"dataset_id": True}},
-                }
-            }
-        }
+        assert query == snapshot
 
-    def test_query_builder_catalogue_deep_inside(self):
+    def test_query_builder_catalogue_deep_inside(self, snapshot):
         query = build_query(
             ({"service_name", "coordinate_unit", "maximum_value"}),
             CopernicusMarineCatalogue,
         )
-        assert query == {
-            "products": {
-                "__all__": {
-                    "datasets": {
-                        "__all__": {
-                            "versions": {
-                                "__all__": {
-                                    "parts": {
-                                        "__all__": {
-                                            "services": {
-                                                "__all__": {
-                                                    "service_name": True,
-                                                    "variables": {
-                                                        "__all__": {
-                                                            "coordinates": {
-                                                                "__all__": {
-                                                                    "coordinate_unit": True,  # noqa: E501
-                                                                    "maximum_value": True,  # noqa: E501
-                                                                }
-                                                            }
-                                                        }
-                                                    },
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        assert query == snapshot
 
     # For the get
-    def test_query_builder_responseget(self):
+    def test_query_builder_responseget(self, snapshot):
         query = build_query(({"files", "files_deleted"}), ResponseGet)
-        assert query == {"files": True, "files_deleted": True}
+        assert query == snapshot
 
-    def test_query_builder_responseget_inside_basemodel(self):
+    def test_query_builder_responseget_inside_basemodel(self, snapshot):
         query = build_query(({"file_status"}), ResponseGet)
-        assert query == {"files": {"__all__": {"file_status": True}}}
+        assert query == snapshot
 
     # For the subset
-    def test_basic_query_builder_responsesubset(self):
+    def test_basic_query_builder_responsesubset(self, snapshot):
         query = build_query(
             ({"file_path", "output_directory"}), ResponseSubset
         )
-        assert query == {"file_path": True, "output_directory": True}
+        assert query == snapshot
 
-    def test_subset_optional_float(self):
+    def test_subset_optional_float(self, snapshot):
         query = build_query(
             ({"file_size", "data_transfer_size"}), ResponseSubset
         )
-        assert query == {"file_size": True, "data_transfer_size": True}
+        assert query == snapshot
 
-    def test_subset_optional_status(self):
+    def test_subset_optional_status(self, snapshot):
         query = build_query(({"status", "file_status"}), ResponseSubset)
-        assert query == {"status": True, "file_status": True}
+        assert query == snapshot
 
-    def test_subset_optional_coordinate_id(self):
+    def test_subset_optional_coordinate_id(self, snapshot):
         query = build_query(({"coordinate_id"}), ResponseSubset)
-        assert query == {
-            "coordinates_extent": {"__all__": {"coordinate_id": True}}
-        }
+        assert query == snapshot
 
-    def test_subset_optional_coordinate_id_and_file(self):
+    def test_subset_optional_coordinate_id_and_file(self, snapshot):
         query = build_query(({"coordinate_id", "file_status"}), ResponseSubset)
-        assert query == {
-            "coordinates_extent": {"__all__": {"coordinate_id": True}},
-            "file_status": True,
-        }
+        assert query == snapshot
 
     def test_return_available_fields(self, snapshot):
         self.when_I_run_with_model("describe", snapshot)
@@ -192,24 +66,13 @@ class TestQueryBuilder:
         self, command: Literal["get", "describe", "subset"], snapshot
     ):
         if command == "describe":
-            fields = "--return-fields"
-            included = {"minimum_value", "one-wrong"}
             model = CopernicusMarineCatalogue
-            name = "COPERNICUS_MARINE_CATALOGUE"
         else:
-            fields = "--response-fields"
-            included = {"file_path", "output_directory", "one-wrong"}
             if command == "get":
                 model = ResponseGet
-                name = "RESPONSE_GET"
             else:
                 model = ResponseSubset
-                name = "RESPONSE_SUBSET"
-        queryable_fields = get_queryable_requested_fields(
-            included, model, fields
-        )
+
         available_fields = _return_available_fields(model)
 
-        check_intersection = queryable_fields.intersection(ALL_FIELDS[name])
-        assert check_intersection == queryable_fields
         assert sorted(list(available_fields)) == snapshot
