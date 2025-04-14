a393b41ecea90edfe74c3db5ff36abea3e06c8a8
uriii3 <oriolrv689@gmail.com>
2025-04-14 16:47:56 +0200

    is intance correctly written

--- copernicusmarine/python_interface/load_utils.py
+++ copernicusmarine/python_interface/load_utils.py
@@ -124,7 +124,7 @@ def load_data_object_from_load_request(
             depth_parameters=load_request.depth_parameters,
             coordinates_selection_method=load_request.coordinates_selection_method,
         )
-        if dataset.isinstance(xarray.Dataset):
+        if isinstance(dataset, xarray.Dataset):
             dataset = dataset.chunk(optimum_dask_chunking)
     else:
         raise ServiceNotSupported(retrieval_service.service_name)
