24a254b06229414c9b11e08733e0614701384bf9
renaudjester <renaud.jester@outlook.fr>
2025-05-21 11:05:43 +0200

    test: update tests

---
+++ tests/resources/mock_stac_catalog/mock_catalog_waw4.py
@@ -0,0 +1,22 @@
+MOCK_CATALOG_WAW4 = {
+    "id": "MDS",
+    "type": "Catalog",
+    "stac_version": "1.0.0",
+    "stac_extensions": [],
+    "title": "Copernicus Marine Data Store",
+    "description": "Data from the Copernicus Marine Data Store, in Analysis-Ready, Cloud-Optimised (ARCO) format.\n",
+    "links": [
+        {
+            "rel": "self",
+            "href": "https://stac.marine.copernicus.eu/metadata/catalog.stac.json",
+            "title": "Copernicus Marine Data Store",
+            "type": "application/json",
+        },
+        {
+            "rel": "root",
+            "href": ".",
+            "title": "Copernicus Marine Data Store",
+            "type": "application/json",
+        },
+    ],
+}
