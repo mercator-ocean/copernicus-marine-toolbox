a438df467c7a6328a0535ed265fa934b1d18534f
uriii3 <oriolrv689@gmail.com>
2025-08-25 13:48:44 +0200

    Copernicus Marine Toolbox Release v

--- copernicusmarine/core_functions/custom_s3_store_zarr_v2.py
+++ copernicusmarine/core_functions/custom_s3_store_zarr_v2.py
@@ -126,6 +126,8 @@ class CustomS3StoreZarrV2(MutableMapping):
         for index_try in range(self.number_of_retries):
             try:
                 return fn()
+            except KeyError:
+                raise
             except Exception as e:
                 if index_try == self.number_of_retries - 1:
                     raise e
