15c406ef78bb17ff731a8b6532480f9a789dceb4
Renaud <38732257+renaudjester@users.noreply.github.com>
2025-10-06 09:11:00 +0200

    fix: chunk size limit 0 fails (#421)

--- tests/test_subset.py
+++ tests/test_subset.py
@@ -1465,6 +1465,21 @@ class TestSubset:
         self.output = execute_in_terminal(command, timeout_second=timeout)
         assert self.output.returncode == 0
 
+    def test_chunking_size_limit_is_0(self, tmp_path):
+        subset(
+            dataset_id="cmems_mod_glo_phy-so_anfc_0.083deg_P1D-m",
+            variables=["so"],
+            minimum_longitude=-13,
+            maximum_longitude=-12,
+            minimum_latitude=47,
+            maximum_latitude=48,
+            start_datetime="2025-09-24T00:00:00",
+            end_datetime="2025-09-25T00:00:00",
+            maximum_depth=0.49402499198913574,
+            chunk_size_limit=0,
+            output_directory=tmp_path,
+        )
+
     def test_requested_interval_is_correct_w_weird_windowing(self, tmp_path):
         output_filename = "output.nc"
         min_longitude = -180.001
