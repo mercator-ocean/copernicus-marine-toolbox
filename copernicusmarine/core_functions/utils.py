37b70f8348158513ac590090b23269f8f8fea17b
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-12-28 12:44:31 +0200

    allow passing `max_concurrent_requests=0` to avoid `ThreadPoolExecutor`

--- copernicusmarine/core_functions/utils.py
+++ copernicusmarine/core_functions/utils.py
@@ -145,17 +145,22 @@ def run_concurrently(
         total=len(function_arguments),
         **tdqm_bar_configuration,
     ) as pbar:
-        with concurrent.futures.ThreadPoolExecutor(
-            max_workers=max_concurrent_requests
-        ) as executor:
-            future_to_url = (
-                executor.submit(func, *function_argument)
-                for function_argument in function_arguments
-            )
-            for future in concurrent.futures.as_completed(future_to_url):
-                data = future.result()
-                out.append(data)
+        if max_concurrent_requests == 0:
+            for function_argument in function_arguments:
+                out.append(func(*function_argument))
                 pbar.update(1)
+        else:
+            with concurrent.futures.ThreadPoolExecutor(
+                max_workers=max_concurrent_requests
+            ) as executor:
+                future_to_url = (
+                    executor.submit(func, *function_argument)
+                    for function_argument in function_arguments
+                )
+                for future in concurrent.futures.as_completed(future_to_url):
+                    data = future.result()
+                    out.append(data)
+                    pbar.update(1)
     return out
 
 
