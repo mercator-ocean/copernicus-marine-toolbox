7ded83cd6a1dd00283205d87da6fd5dce5f0801c
uriii3 <oriolrv689@gmail.com>
2025-12-30 16:42:46 +0100

    improving tests and solving errors

--- tests/test_get_create_file_list.py
+++ tests/test_get_create_file_list.py
@@ -22,7 +22,7 @@ class TestGetCreateFileList:
             in self.output.stderr
         )
 
-    def test_get_create_file_list(self):
+    def test_get_create_file_list(self, tmp_path):
         self.command = [
             "copernicusmarine",
             "get",
@@ -30,13 +30,15 @@ class TestGetCreateFileList:
             "cmems_mod_ibi_phy-temp_my_0.027deg_P1M-m",
             "--create-file-list",
             "hello.txt",
+            "--output-directory",
+            f"{tmp_path}",
         ]
         self.output = execute_in_terminal(self.command)
-        assert os.path.isfile("hello.txt")
-        with open("hello.txt") as file:
+        assert os.path.isfile(tmp_path / "hello.txt")
+        with open(tmp_path / "hello.txt") as file:
             assert file.read() != ""
 
-    def test_get_create_file_list_csv(self):
+    def test_get_create_file_list_csv(self, tmp_path):
         self.command = [
             "copernicusmarine",
             "get",
@@ -44,10 +46,12 @@ class TestGetCreateFileList:
             "cmems_mod_ibi_phy-mflux_my_0.027deg_P1M-m",
             "--create-file-list",
             "hello.csv",
+            "--output-directory",
+            f"{tmp_path}",
         ]
         self.output = execute_in_terminal(self.command)
-        assert os.path.isfile("hello.csv")
-        with open("hello.csv") as file:
+        assert os.path.isfile(tmp_path / "hello.csv")
+        with open(tmp_path / "hello.csv") as file:
             assert (
                 file.readline()
                 == "filename,size,last_modified_datetime,etag\n"
@@ -65,41 +69,46 @@ class TestGetCreateFileList:
             == "Download file list must be a '.txt' or '.csv' file. "
         )
 
-    def test_get_create_file_list_python(self):
+    def test_get_create_file_list_python(self, tmp_path):
         get(
             dataset_id="cmems_mod_ibi_phy-sal_my_0.027deg_P1M-m",
             create_file_list="hello_python.txt",
+            output_directory=tmp_path,
         )
-        assert os.path.isfile("hello_python.txt")
-        with open("hello_python.txt") as file:
+        assert os.path.isfile(tmp_path / "hello_python.txt")
+        with open(tmp_path / "hello_python.txt") as file:
             assert file.read() != ""
 
-    def test_get_create_file_list_csv_python(self):
+    def test_get_create_file_list_csv_python(self, tmp_path):
         get(
             dataset_id="cmems_mod_ibi_phy-ssh_my_0.027deg_P1M-m",
             create_file_list="hello_python.csv",
+            output_directory=tmp_path,
         )
-        assert os.path.isfile("hello_python.csv")
-        with open("hello_python.csv") as file:
+        assert os.path.isfile(tmp_path / "hello_python.csv")
+        with open(tmp_path / "hello_python.csv") as file:
             assert (
                 file.readline()
                 == "filename,size,last_modified_datetime,etag\n"
             )
 
-    def test_get_create_file_with_overwrite_option(self):
+    def test_get_create_file_with_overwrite_option(self, tmp_path):
         get(
             dataset_id="cmems_mod_ibi_phy-temp_my_0.027deg_P1M-m",
             create_file_list="hello_python_overwrite.txt",
+            output_directory=tmp_path,
         )
         get(
             dataset_id="cmems_mod_ibi_phy-temp_my_0.027deg_P1M-m",
             create_file_list="hello_python_overwrite.txt",
             overwrite=True,
+            output_directory=tmp_path,
         )
-        assert not os.path.isfile("hello_python_overwrite_(1).txt")
+        assert not os.path.isfile(tmp_path / "hello_python_overwrite_(1).txt")
 
         get(
             dataset_id="cmems_mod_ibi_phy-wcur_my_0.027deg_P1M-m",
             create_file_list="hello_python_overwrite.txt",
+            output_directory=tmp_path,
         )
-        assert os.path.isfile("hello_python_overwrite_(1).txt")
+        assert os.path.isfile(tmp_path / "hello_python_overwrite_(1).txt")
